9cbfd00f8c8c5382aeac672ffb89481352508ae1
Ben Szucs <benszucs@gmail.com>
2018-12-03 20:56:54 +0000

    Start about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import ben from '../assets/images/ben.png';
 
 const About = () => (
   <div className="page--container">
     <h1 className="page--title">ABOUT</h1>
+    <div className="about-grid">
+      <div className="about-grid-item">
+        <img
+          className="about-image"
+          src={ben}
+          alt="Photo of Ben"
+        />
+      </div>
+
+      <div className="about-grid-item">
+        <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+        <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+      </div>
+
+      <div className="about-grid-item">
+        <div className="about-skills-container">
+          <h3 className="about-skills-title">SKILLS</h3>
+          <div className="about-skills">
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>Sass</span>
+            <span>JavaScript</span>
+            <span>ES6+</span>
+            <span>Git</span>
+            <span>React</span>
+            <span>Redux</span>
+            <span>Command Line</span>
+            <span>npm/yarn</span>
+            <span>Webpack</span>
+            <span>Babel</span>
+            <span>JSON</span>
+            <span>AJAX</span>
+            <span>XML</span>
+            <span>Bootstrap</span>
+            <span>jQuery</span>
+            <span>Heroku</span>
+            <span>Node.js</span>
+            <span>Express</span>
+            <span>Mobile First</span>
+            <span>Cross Browser</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 );
 
--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import Tilt from 'react-tilt';
+
 //ICONS
 import BSZLogo from './icons/BSZLogo.js';
 import YellowMoon from './icons/YellowMoon.js';
@@ -9,80 +11,80 @@ import ToDoIcon from './icons/ToDoIcon';
 import QuoteGenIcon from './icons/QuoteGenIcon';
 import MovieIcon from './icons/MovieIcon';
 
-// import Project from './pages/Project';
-
 
 export default class ProjectGrid extends Component {
-  changeSVGFillonHover
+
   render() {
     return (
       <div className="grid">
         {this.props.projects.projectsArray.map((project, index) => (
+          <div className="grid--item-container">
+            <Tilt className="Tilt" options={{ max: 25, scale: 1.1 }} style={{ height: "80%", width: "80%" }}>
+              <div key={index} className="grid--item Tilt-inner">
 
-          <div key={index} className="grid--item">
+                {index === 0 &&
+                  <div className="grid--icon">
+                    <div className="bsz-logo"><BSZLogo /></div>
+                    <div className="bsz-yellow-moon"><YellowMoon /></div>
+                    <div className="bsz-blue-moon"><BlueMoon /></div>
+                  </div>
+                }
 
-            {index === 0 &&
-              <div className="grid--icon">
-                <div className="bsz-logo"><BSZLogo /></div>
-                <div className="bsz-yellow-moon"><YellowMoon /></div>
-                <div className="bsz-blue-moon"><BlueMoon /></div>
+                {index === 1 &&
+                  <div className="grid--icon">
+                    <ToDoIcon />
+                  </div>
+                }
+
+                {index === 2 &&
+                  <div className="grid--icon">
+                    <QuoteGenIcon />
+                  </div>
+                }
+
+                {index === 3 &&
+                  <div className="grid--icon">
+                    <MovieIcon />
+                  </div>
+                }
+
+
+              <div className="grid--title-container">
+                <div className="grid--title anim-title">
+                  <Link
+                    className="title__color-trans"
+                    to={`/projects`}
+                  >
+                    <h3>{project.title}</h3>
+                  </Link>
+                </div>
+
+                <div className="grid--title anim-links">
+                  <a
+                    className="title__color-trans"
+                    href={project.demoLink}
+                    rel="noopener noreferrer"
+                  >
+                    <h3>Demo</h3>
+                  </a>
+                  <a
+                    className="title__color-trans"
+                    href={project.githubLink}
+                    rel="noopener noreferrer"
+                  >
+                    <h3>Code</h3>
+                  </a>
+                </div>
+
+                <div className="grid--title anim-title-appear">
+                  <h3>{project.title}</h3>
+                </div>
               </div>
-            }
 
-            {index === 1 &&
-              <div className="grid--icon">
-                <ToDoIcon />
+
               </div>
-            }
-
-            {index === 2 &&
-              <div className="grid--icon">
-                <QuoteGenIcon />
-              </div>
-            }
-
-            {index === 3 &&
-              <div className="grid--icon">
-                <MovieIcon />
-              </div>
-            }
-
-
-          <div className="grid--title-container">
-            <div className="grid--title anim-title">
-              <Link
-                className="title__color-trans"
-                to={`/projects`}
-              >
-                <h3>{project.title}</h3>
-              </Link>
-            </div>
-
-            <div className="grid--title anim-links">
-              <a
-                className="title__color-trans"
-                href={project.demoLink}
-                rel="noopener noreferrer"
-              >
-                <h3>Demo</h3>
-              </a>
-              <a
-                className="title__color-trans"
-                href={project.githubLink}
-                rel="noopener noreferrer"
-              >
-                <h3>Code</h3>
-              </a>
-            </div>
-
-            <div className="grid--title anim-title-appear">
-              <h3>{project.title}</h3>
-            </div>
+            </Tilt>
           </div>
-
-
-          </div>
-
         ))}
       </div>
     );
